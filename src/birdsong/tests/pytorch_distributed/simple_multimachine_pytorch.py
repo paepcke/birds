ea5432c8ff565f4206d44ed22e2ff85ec9a112d6
Andreas Paepcke <paepcke@cs.stanford.edu>
2021-01-12 18:36:16 -0800

    Started very simple test for pytorch inter-machine connectivity

--- src/birdsong/tests/pytorch_distributed/simple_multimachine_pytorch.py
+++ src/birdsong/tests/pytorch_distributed/simple_multimachine_pytorch.py
@@ -5,11 +5,11 @@ Created on Jan 12, 2021
 '''
 
 import os
+from subprocess import Popen
+import subprocess
 import sys
-import time
 
-import torch
-from torch.multiprocessing import Process
+#*****from torch.multiprocessing import Process
 
 import torch.distributed as dist
 
@@ -23,39 +23,31 @@ class PytorchDistribTester:
         self.local_rank  = int(sys.argv[3])
         self.world_size  = int(sys.argv[4])
         self.master_port = 5678
-        
-        if   dist.is_nccl_available():
-            self.backend = 'nccl'
-        elif dist.is_gloo_available():
-            self.backend = 'gloo'
-            
-        print(f"Using backend: {self.backend}")
-    
-    def fake_train_script(self, rank, world_size):
-        """ Distributed function to be implemented later. """
-        while True:
-            print(f"Training (Rank: {rank}; world_size: {world_size})")
-            time.sleep(2)
-
+   
     def run(self):
         
-        processes = []
-        for rank in range(self.world_size):
-            p = Process(target=self.init_process)
-            p.start()
-            processes.append(p)
-    
-        for p in processes:
-            p.join()    
+        curr_dir = os.path.dirname(__file__)
+        script_path = os.path.join(curr_dir, 'fake_train_script.py')
+        p = Popen([script_path,
+                   self.master_addr,
+                   self.rank,
+                   self.local_rank,
+                   self.world_size
+                   ], 
+                   stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE
+                   )
+        p.wait()
+        
 
-    def init_process(self, backend='nccl'):
-        """ Initialize the distributed environment. """
-        os.environ['MASTER_ADDR'] = str(self.master_addr)
-        os.environ['MASTER_PORT'] = str(self.master_port)
-        dist.init_process_group(self.backend, 
-                                rank=self.rank, 
-                                world_size=self.world_size)
-        self.fake_train_script(self.rank, self.world_size)
+#     def init_process(self, rank):
+#         """ Initialize the distributed environment. """
+#         os.environ['MASTER_ADDR'] = str(self.master_addr)
+#         os.environ['MASTER_PORT'] = str(self.master_port)
+#         dist.init_process_group(self.backend, 
+#                                 rank=self.rank, 
+#                                 world_size=self.world_size)
+#         self.fake_train_script(self.rank, self.world_size)
     
     
 if __name__ == '__main__':
