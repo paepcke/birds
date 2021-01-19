5d96f399260e4ea79c2e1b1fac39612ec1f943ed
Andreas Paepcke <paepcke@cs.stanford.edu>
2021-01-19 10:43:43 -0800

    Built up the minimal ddp program to ressemble what we do

--- src/birdsong/minimal_ddp.py
+++ src/birdsong/minimal_ddp.py
@@ -6,6 +6,8 @@ Created on Jan 19, 2021
 import os
 import sys
 import tempfile
+import subprocess
+
 import torch
 import torch.distributed as dist
 import torch.nn as nn
@@ -14,32 +16,37 @@ import torch.multiprocessing as mp
 
 from torch.nn.parallel import DistributedDataParallel as DDP
 
+#*****************
+# import socket 
+# if socket.gethostname() in ('quintus', 'quatro'):
+#     # Point to where the pydev server 
+#     # software is installed on the remote
+#     # machine:
+#     sys.path.append(os.path.expandvars("$HOME/Software/Eclipse/PyDevRemote/pysrc"))
+
+#     import pydevd
+#     global pydevd
+#     # Uncomment the following if you
+#     # want to break right on entry of
+#     # this module. But you can instead just
+#     # set normal Eclipse breakpoints:
+#     #*************
+#     print("About to call settrace()")
+#     #*************
+#     pydevd.settrace('localhost', port=4040)
+# **************** 
 
 class MinimalDDP:
 
     def setup(self, rank, world_size):
-        if sys.platform == 'win32':
-            # Distributed package only covers collective communications with Gloo
-            # backend and FileStore on Windows platform. Set init_method parameter
-            # in init_process_group to a local file.
-            # Example init_method="file:///f:/libtmp/some_file"
-            init_method="file:///{your local file path}"
-    
-            # initialize the process group
-            dist.init_process_group(
-                "gloo",
-                init_method=init_method,
-                rank=rank,
-                world_size=world_size
-            )
-        else:
-            os.environ['MASTER_ADDR'] = 'localhost'
-            os.environ['MASTER_PORT'] = '12355'
-    
-            # initialize the process group
-            dist.init_process_group("gloo", rank=rank, world_size=world_size)
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = '12355'
+
+        # initialize the process group
+        dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
     def demo_basic(self, rank, world_size):
+            
         print(f"Running basic DDP example on rank {rank}.")
         self.setup(rank, world_size)
     
@@ -58,17 +65,6 @@ class MinimalDDP:
     
         self.cleanup()
 
-    def run_demo(self, demo_fn, world_size):
-        mp.spawn(demo_fn,
-                 args=(world_size,),
-                 nprocs=world_size,
-                 join=True)
-    
-    
-    def cleanup(self):
-        dist.destroy_process_group()
-
-
 class ToyModel(nn.Module):
     def __init__(self):
         super(ToyModel, self).__init__()
@@ -82,5 +78,7 @@ class ToyModel(nn.Module):
 # ------------------------ Main ------------
 if __name__ == '__main__':
 
+    rank       = sys.argv[1]
+    world_size = sys.argv[2]
     min_ddp = MinimalDDP()
-    min_ddp.run_demo(min_ddp.demo_basic, 1)
+    min_ddp.demo_basic(rank, world_size)
---
+++ src/birdsong/minimal_ddp_launcher.py
@@ -0,0 +1,57 @@
+'''
+Created on Jan 19, 2021
+
+@author: paepcke
+'''
+import os
+import sys
+import tempfile
+import subprocess
+
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
+import torch.multiprocessing as mp
+
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+#*****************
+# import socket 
+# if socket.gethostname() in ('quintus', 'quatro'):
+#     # Point to where the pydev server 
+#     # software is installed on the remote
+#     # machine:
+#     sys.path.append(os.path.expandvars("$HOME/Software/Eclipse/PyDevRemote/pysrc"))
+
+#     import pydevd
+#     global pydevd
+#     # Uncomment the following if you
+#     # want to break right on entry of
+#     # this module. But you can instead just
+#     # set normal Eclipse breakpoints:
+#     #*************
+#     print("About to call settrace()")
+#     #*************
+#     pydevd.settrace('localhost', port=4040)
+# **************** 
+
+class MinimalDDPLauncher:
+   
+    def run_demo(self, demo_fn, world_size):
+        # mp.spawn(demo_fn,
+        #          args=(world_size,),
+        #          nprocs=world_size,
+        #          join=True)
+        procs = []
+        for i in range(world_size):
+            print(f"Starting {demo_fn}[{i}] of {world_size}")
+            procs.append(subprocess.Popen(['python3', demo_fn, i, world_size]))
+
+# ------------------------ Main ------------
+if __name__ == '__main__':
+
+    curr_dir = os.path.dirname(__file__)
+    script_path = os.path.join(curr_dir, 'minimal_ddp.py')
+    launcher = MinimalDDPLauncher()
+    launcher.run_demo(script_path, 2)
