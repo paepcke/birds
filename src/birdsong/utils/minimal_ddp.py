55f03d1d565395dda633c69c8f707a451a1f0eed
Andreas Paepcke <paepcke@cs.stanford.edu>
2021-01-22 20:05:03 -0800

    Added tracing to minimal_ddp.py

--- src/birdsong/utils/minimal_ddp.py
+++ src/birdsong/utils/minimal_ddp.py
@@ -11,6 +11,27 @@ import torch.optim as optim
 
 from torch.nn.parallel import DistributedDataParallel as DDP
 
+#*****************
+#
+import socket
+if socket.gethostname() in ('quintus', 'quatro'):
+    # Point to where the pydev server
+    # software is installed on the remote
+    # machine:
+    sys.path.append(os.path.expandvars("$HOME/Software/Eclipse/PyDevRemote/pysrc"))
+
+    import pydevd
+    global pydevd
+    # Uncomment the following if you
+    # want to break right on entry of
+    # this module. But you can instead just
+    # set normal Eclipse breakpoints:
+    #*************
+    print("About to call settrace()")
+    #*************
+    pydevd.settrace('localhost', port=4040)
+#****************
+
 class MinimalDDP:
     '''Test whether DDP really does something'''
     
