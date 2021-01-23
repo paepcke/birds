1013595937eb1ad97f9ef9f028fc510a6882230d
Andreas Paepcke <paepcke@cs.stanford.edu>
2021-01-22 21:56:30 -0800

    Split minimal ddp test into minimal_across_two_gpus_ddp.py and minimal_within_two_gpus_ddp.py

---
+++ src/birdsong/utils/minimal_across_two_gpus_ddp.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import copy
+
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
+from torch import randn
+
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+#*****************
+#
+import socket
+if socket.gethostname() in ('quintus', 'quatro'):
+    # Point to where the pydev server
+    # software is installed on the remote
+    # machine:
+    sys.path.append(os.path.expandvars("$HOME/Software/Eclipse/PyDevRemote/pysrc"))
+
+    import pydevd
+    global pydevd
+    # Uncomment the following if you
+    # want to break right on entry of
+    # this module. But you can instead just
+    # set normal Eclipse breakpoints:
+    #*************
+    print("About to call settrace()")
+    #*************
+    pydevd.settrace('localhost', port=4040)
+#****************
+
+class MinimalDDP:
+    '''Test whether DDP really does something'''
+    
+    epochs  = 2
+    batches = 3
+
+    #------------------------------------
+    # setup
+    #-------------------
+
+    def setup(self, rank, world_size):
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = '12355'
+
+        # initialize the process group
+        dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+    #------------------------------------
+    # demo_basic
+    #-------------------
+
+    def demo_basic(self, rank, world_size, model_save_dir='/tmp'):
+        '''The action: train model; save intermediate states'''
+            
+        print(f"Running basic DDP example on rank {rank}.")
+        self.setup(rank, world_size)
+    
+        # create model and move it to GPU with id rank
+        model = ToyModel().to(rank)
+        ddp_model = DDP(model, device_ids=[rank])
+    
+        loss_fn = nn.MSELoss()
+        optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+
+        for _epoch in range(self.epochs):
+            for _i in range(self.batches):
+                
+                optimizer.zero_grad()
+                outputs = ddp_model(randn(20, 10).to(rank))
+                labels = randn(20, 5).to(rank)
+                
+                before_model = ddp_model.cpu()
+                before_state = copy.deepcopy(before_model.state_dict())
+                torch.save(before_state, f"/tmp/before_rank{rank}.pth")
+                ddp_model.to(rank)
+                
+                loss_fn(outputs, labels).backward()
+                optimizer.step()
+
+                after_model = ddp_model.cpu()
+                after_state = after_model.state_dict()
+                torch.save(after_state, f"/tmp/after_rank{rank}.pth")
+                ddp_model.to(rank)
+                                
+                dist.barrier()
+                
+                # Read the other's before/after states:
+                other_rank = 1-rank
+                other_before_state = torch.load(f"/tmp/before_rank{other_rank}.pth")
+                other_after_state  = torch.load(f"/tmp/after_rank{other_rank}.pth")                
+                
+                # Before states should be different:
+                states_equal = True
+                for before_parm, after_parm in zip(other_before_state.values(),
+                                                   before_state.values()):
+                    if before_parm.ne(after_parm).any():
+                        states_equal = False
+
+                print(f"Before states across gpus are {('equal' if states_equal else 'different')}")
+
+
+                # After states should be the same:
+                states_equal = True
+                for after_parm_other, after_parm in zip(other_after_state.values(),
+                                                   after_state.values()):
+                    if after_parm_other.ne(after_parm).any():
+                        states_equal = False
+
+                print(f"After states across gpus are {('equal' if states_equal else 'different')}")
+
+                # Clean GPU memory:
+                outputs.cpu()
+                labels.cpu()
+
+        dist.barrier()
+
+        self.cleanup()
+
+    #------------------------------------
+    # cleanup 
+    #-------------------
+
+    def cleanup(self):
+        dist.destroy_process_group()
+        print(f"Rank {rank} is done.")
+        
+# ------------------------ Toy Model ----------
+
+class ToyModel(nn.Module):
+    def __init__(self):
+        super(ToyModel, self).__init__()
+        self.net1 = nn.Linear(10, 10)
+        self.relu = nn.ReLU()
+        self.net2 = nn.Linear(10, 5)
+
+    def forward(self, x):
+        return self.net2(self.relu(self.net1(x)))
+
+# ------------------------ Main ------------
+if __name__ == '__main__':
+
+    # Started via minimal_ddp_launcher.py,
+    # which sets the rank:
+    
+    rank           = int(sys.argv[1])
+    world_size     = 2
+    min_ddp = MinimalDDP().demo_basic(rank, world_size)
