1fed74dda005423d63cde271e719afc0483f62f5
Andreas Paepcke <paepcke@cs.stanford.edu>
2021-03-05 16:59:58 -0800

    Turned off remote debug

--- src/birdsong/birds_train_basic.py
+++ src/birdsong/birds_train_basic.py
@@ -37,22 +37,22 @@ from birdsong.result_tallying import EpochSummary
 
 #*****************
 # 
-if socket.gethostname() in ('quintus', 'quatro', 'sparky'):
-    # Point to where the pydev server 
-    # software is installed on the remote
-    # machine:
-    sys.path.append(os.path.expandvars("$HOME/Software/Eclipse/PyDevRemote/pysrc"))
-       
-    import pydevd
-    global pydevd
-    # Uncomment the following if you
-    # want to break right on entry of
-    # this module. But you can instead just
-    # set normal Eclipse breakpoints:
-    #*************
-    print("About to call settrace()")
-    #*************
-    pydevd.settrace('localhost', port=4040)
+# if socket.gethostname() in ('quintus', 'quatro', 'sparky'):
+#     # Point to where the pydev server 
+#     # software is installed on the remote
+#     # machine:
+#     sys.path.append(os.path.expandvars("$HOME/Software/Eclipse/PyDevRemote/pysrc"))
+#        
+#     import pydevd
+#     global pydevd
+#     # Uncomment the following if you
+#     # want to break right on entry of
+#     # this module. But you can instead just
+#     # set normal Eclipse breakpoints:
+#     #*************
+#     print("About to call settrace()")
+#     #*************
+#     pydevd.settrace('localhost', port=4040)
 # **************** 
 
 class BirdsTrainBasic:
