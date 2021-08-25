a28ef907b8c97251c9dd84efbfd57ca9a7fbbd8c
Andreas Paepcke <paepcke@cs.stanford.edu>
2021-08-24 17:10:32 -0700

    Now require ml-experiment-manager >= 0.1.6

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
                         'discrete-differentiator>=0.1',
                         'scikit-image>=0.18.1',
                         'skorch>=0.10.0',
-                        'ml-experiment-manager>=0.1.5',
+                        'ml-experiment-manager>=0.1.6',
                         ],
 
     tests_require    =[
