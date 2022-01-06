000ef119c84836e9dde5f819d790424038750843
Andreas Paepcke <paepcke@cs.stanford.edu>
2022-01-06 15:50:22 -0800

    Require ml_eperiment_manager 0.1.13

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
                         'discrete-differentiator>=0.1',
                         'scikit-image>=0.18.1',
                         'skorch>=0.10.0',
-                        'ml-experiment-manager>=0.1.12',
+                        'ml-experiment-manager>=0.1.13',
                         'mplcursors>=0.5',
                         'tabulate>=0.8.9',
                         #'mtspec>=0.3.2',
